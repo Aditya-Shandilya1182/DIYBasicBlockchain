67a5dede27ec606a381c9b10f9dbb5275511cd8e
Maverick18 <115613425+Aditya-Shandiya1812@users.noreply.github.com>
2023-04-26 19:45:34 +0530

    block part done

--- index.ts
+++ index.ts
@@ -11,3 +11,20 @@ class Transaction {
     return JSON.stringify(this);
   }
 }
+
+class Block {
+    public nonce = Math.round(Math.random() * 999999999);
+
+  constructor(
+    public prevHash: string, 
+    public transaction: Transaction, 
+    public ts = Date.now()
+  ) {}
+
+  get hash() {
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash('SHA256');
+    hash.update(str).end();
+    return hash.digest('hex');
+  }
+}
